d4a6b53fe6a9855526202ec38201afa87b064275
DasSworx <158160735+DasSworx@users.noreply.github.com>
2025-10-19 20:19:23 +0200

    Added verbose parameter to errors for clean disabling

--- errors.py
+++ errors.py
@@ -1,44 +1,73 @@
-class NotAPacketError(Exception):
-    pass
+#verbose Levels: 
+#0: only critical errors
+#3: Warning
+#4: Info
+#5: Debug
 
-class InvalidKNXPacketError(Exception):
-    pass
+import configparser
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-class InvalidChecksumError(Exception):
-    pass
-
-class networkToSmallError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe size of the network provided is not large enough. Minimum required is /16")
-
-class cEMIFrameCouldNotBeMappedError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe mapping of the provided cEMI-frame failed")
-
-class payloadIsEmptyError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe raw data segment of the package had a length of zero")
-
-class noRawDataFoundError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe package did not contain any raw data")
-
-class noUDPFoundError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe package did not contain a UDP payload")
-
-class wrongUDPPortError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe port used by UDP does not belong to KNX")
-
-class TelegramTypeNotSupportedError(Exception):
-    def __init__(self):
-        super().__init__("\n\tThe control byte of the provided telegram does not match any supported type")
+#CRITICAL ERRORS
 
 class notAMapperError(Exception):
     def __init__(self):
-        super().__init__("\n\tThe chosen Mapper could no be found, please verify it exists \n\tAlternativly change the Mapper in the config.ini")
+        super().__init__()
+        print("\n\tThe chosen Mapper could no be found, please verify it exists \n\tAlternativly change the Mapper in the config.ini")
+
+class networkToSmallError(Exception):
+    def __init__(self):
+        super().__init__()
+        print("\n\tThe size of the network provided is not large enough. Minimum required is /16")
+
+#WARNINGS
 
 class TelegramDoesNotUseSequenceNumberError(Exception):
     def __init__(self):
-        super().__init__("The telegram provided uses a service code, that does not use Sequence Numbers!")
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 2:
+            print("The telegram provided uses a service code, that does not use Sequence Numbers!")
+
+class cEMIFrameCouldNotBeMappedError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 2:
+            print("\n\tThe mapping of the provided cEMI-frame failed")
+
+#INFO
+
+class NotAPacketError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tData received cannot be resolved to a packet")
+
+class payloadIsEmptyError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tThe raw data segment of the package had a length of zero")
+
+class noRawDataFoundError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tThe package did not contain any raw data")
+
+class noUDPFoundError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tThe package did not contain a UDP payload")
+
+class wrongUDPPortError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tThe port used by UDP does not belong to KNX")
+
+class TelegramTypeNotSupportedError(Exception):
+    def __init__(self):
+        super().__init__()
+        if int(config["Settings"]["verbose"]) < 3:
+            print("\n\tThe control byte of the provided telegram does not match any supported type")
