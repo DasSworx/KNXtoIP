c733d5edd946d1ba678752b09a8f4796385a23e7
DasSworx <nachtigaller@protonmail.com>
2025-10-20 11:36:03 +0200

    Changed < to > so verbose actually works

--- errors.py
+++ errors.py
@@ -27,13 +27,13 @@ class networkToSmallError(Exception):
 class TelegramDoesNotUseSequenceNumberError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 1:
+        if int(config["Settings"]["verbose"]) > 1:
             print("\n\tWARNING: The telegram provided uses a service code, that does not use Sequence Numbers!")
 
 class cEMIFrameCouldNotBeMappedError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 1:
+        if int(config["Settings"]["verbose"]) > 1:
             print("\n\tWARNING: The mapping of the provided cEMI-frame failed")
 
 #INFO
@@ -41,67 +41,67 @@ class cEMIFrameCouldNotBeMappedError(Exception):
 class NotAPacketError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: Data received cannot be resolved to a packet")
 
 class payloadIsEmptyError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: The raw data segment of the package had a length of zero")
 
 class noRawDataFoundError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: The package did not contain any raw data")
 
 class noUDPFoundError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: The package did not contain a UDP payload")
 
 class wrongUDPPortError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: The port used by UDP does not belong to KNX")
 
 class TelegramTypeNotSupportedError(Exception):
     def __init__(self):
         super().__init__()
-        if int(config["Settings"]["verbose"]) < 2:
+        if int(config["Settings"]["verbose"]) > 2:
             print("\n\tINFO: The control byte of the provided telegram does not match any supported type")
 
 #DEBUG
 
 def newPacketMessage():
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("New Packet")
 
 def newKNXTelegramMessage():
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("NEW KNX_Telegram")
 
 def packetSentMessage():
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("Packet sent!")
 
 def decryptingMessage():
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("----Decrypting:----")
 
 def printAPCI(APCI):
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("----APCI: ----")
         u.print_bytes_as_hex(APCI)
 
 def printPayload(payload):
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("----Payload: ----")
         u.print_bytes_as_hex(payload)
 
 def sendingTestMessage(seq_nr):
-    if int(config["Settings"]["verbose"]) < 3:
+    if int(config["Settings"]["verbose"]) > 3:
         print("Sending message" + str(seq_nr))
